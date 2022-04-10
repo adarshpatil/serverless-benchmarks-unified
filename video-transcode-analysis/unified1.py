f55b7e27968922f39fe9ae048f97d84511e5bc1f
adarshpatil <adarshpatil123@gmail.com>
2022-04-10 22:38:04 +0100

    added get for video functions

--- video-transcode-analysis/unified1.py
+++ video-transcode-analysis/unified1.py
@@ -1,7 +1,26 @@
 import cv2
 import random
+import os
+import shutil
+import tempfile
 
+"""
+This benchmark represents the "Split and transcode video using massive parallelization"
+from Thomson Reuters that uses AWS Step Functions and AWS Lambda
+https://aws.amazon.com/step-functions/use-cases/
+
+To simulate a get of the video file from disaggr mem to local mem
+we simply make a copy of the input file to /dev/shm, which is a RAM-backed filesystem
+https://stackoverflow.com/questions/40290770/opencv-loading-video-in-the-memory
+"""
 def locateframe(video_path, num_segments):
+    ### get begin
+    tempdir = tempfile.TemporaryDirectory(dir='/dev/shm')
+    copypath = os.path.join(tempdir.name, os.path.basename(video_path))
+    shutil.copy(video_path, copypath)
+    ### get end
+    
+    ### compute begin
     video = cv2.VideoCapture(video_path)
     frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     video.release()
@@ -9,11 +28,21 @@ def locateframe(video_path, num_segments):
     keyframes = []
     for i in range(0,num_segments):
         keyframes.append(int(frames/num_segments)*(i+1))
-    print(keyframes)
+    ### compute end
+    
+    #print(keyframes)
+    # TODO pickle keyframes for PUT (very small latency compared to get of video)
     return keyframes
 
 
 def split(video_path, keyframes):
+    ### get begin
+    tempdir = tempfile.TemporaryDirectory(dir='/dev/shm')
+    copypath = os.path.join(tempdir.name, os.path.basename(video_path))
+    shutil.copy(video_path, copypath)
+    ### get end
+    
+    ### compute begin
     video = cv2.VideoCapture(video_path)
 
     width = int(video.get(3))
@@ -46,11 +75,20 @@ def split(video_path, keyframes):
             break
     
     video.release
-    print(segments)
+    ### compute end
+    
+    #print(segments)
     return segments
 
 
-def process(seg_id, video_path):
+def process(seg_id, video_path, keyframes):
+    ### get begin
+    tempdir = tempfile.TemporaryDirectory(dir='/dev/shm')
+    copypath = os.path.join(tempdir.name, os.path.basename(video_path))
+    shutil.copy(video_path, copypath)
+    ### get end
+    
+    ### compute begin
     video = cv2.VideoCapture(video_path)
     
     width = int(video.get(3))
@@ -69,30 +107,43 @@ def process(seg_id, video_path):
     
     out.release
     video.release
+    ### compute end    
     
-    #randomly decide if this segment should be part of concat
+    # analyse should run video analysis for profanity sensor=1 else 0
+    # instead we randomly decide if this segment should be part of concat
     #sensor_segment = random.randint(0,1)
     sensor_segment = 0
     return (seg_out,sensor_segment)
 
 def validate(processed_segments):
+    ### compute begin
     validated_seg = []
     for seg,sensor in processed_segments:
         if sensor == False:
             validated_seg.append(seg)
+    ### compute end
+                
     return validated_seg
 
 
-def concat(validated_seg):
-    if(len(validated_seg)==0):
+def concat(segment_metadata):
+    ### get begin
+    for s in segment_metadata:
+        tempdir = tempfile.TemporaryDirectory(dir='/dev/shm')
+        copypath = os.path.join(tempdir.name, os.path.basename(s))
+        shutil.copy(s, copypath)
+    ### get end
+    
+    ### compute begin
+    if(len(segment_metadata)==0):
         return
-    video = cv2.VideoCapture(validated_seg[0])
+    video = cv2.VideoCapture(segment_metadata[0])
     width = int(video.get(3))
     height = int(video.get(4))
     video.release()
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter("concat.avi", fourcc, 20.0, (width, height))
-    for s in validated_seg:
+    for s in segment_metadata:
         video = cv2.VideoCapture(s)
         while video.isOpened():
             ret, frame = video.read()
@@ -103,19 +154,20 @@ def concat(validated_seg):
         video.release()
 
     out.release()
-    
+    ### compute end
+
 
 video_path = "data/big_buck_bunny_720p_2mb.mp4"
 #video_path = "data/SampleVideo_1280x720_10mb.mp4"
 num_segments = 2
 
-keyframes = locateframe(video_path, num_segments)
-segments = split(video_path, keyframes)
+segment_metadata = locateframe(video_path, num_segments)
+segments = split(video_path, segment_metadata)
 
 processed_segments = []
 for i,s in enumerate(segments):
-    processed_segments.append(process(i,s))
+    processed_segments.append(process(i,s, segment_metadata))
 
-validated_seg = validate(processed_segments)
-print(validated_seg)
-concat(validated_seg)
+segment_metadata = validate(processed_segments)
+#print(segment_metadata)
+concat(segment_metadata)
